99e8ad886a48910ddc7568d75e82a49c45455d42
YunaSan <70344578+TylerLeCmd@users.noreply.github.com>
2024-08-19 12:38:40 -0600

    committing this so that the errors go away and all of my problems are solved!
    beans!

--- scenes/game_screens/level_select.gd
+++ scenes/game_screens/level_select.gd
@@ -8,17 +8,26 @@ func _ready():
 	$PlayerIcon.global_position = levels.global_position
 
 func _input(event):
+	var tween = get_tree().create_tween()
 	if event.is_action_pressed("ui_left") and levels.next_level_left:
 			levels = levels.next_level_left
+			tween.tween_property($PlayerIcon, "position", levels.position, 0.1)
+			await get_tree().create_timer(0.1).timeout
 			$PlayerIcon.global_position = levels.global_position
 	if event.is_action_pressed("ui_right") and levels.next_level_right:
 			levels = levels.next_level_right
+			tween.tween_property($PlayerIcon, "position", levels.position, 0.1)
+			await get_tree().create_timer(0.1).timeout
 			$PlayerIcon.global_position = levels.global_position
 	if event.is_action_pressed("ui_up") and levels.next_level_up:
 			levels = levels.next_level_up
+			tween.tween_property($PlayerIcon, "position", levels.position, 0.1)
+			await get_tree().create_timer(0.1).timeout
 			$PlayerIcon.global_position = levels.global_position
 	if event.is_action_pressed("ui_down") and levels.next_level_down:
 			levels = levels.next_level_down
+			tween.tween_property($PlayerIcon, "position", levels.position, 0.1)
+			await get_tree().create_timer(0.1).timeout
 			$PlayerIcon.global_position = levels.global_position
 	if event.is_action_pressed("ui_accept"):
 		if levels.next_scene_path:
