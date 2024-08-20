2af9e75e78eba65bb817f88b0e61dd6b4d0eaad6
FarhanZaman1895 <117427908+FarhanZaman1895@users.noreply.github.com>
2024-08-20 06:01:17 -0600

    wefwef

--- scripts/spagethie.gd
+++ scripts/spagethie.gd
@@ -1,20 +1,8 @@
 extends Node
 @onready var area: StaticBody2D = $StaticBody2D
 
-var check1 = false
-var check2 = false
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	check1 = true
+	area.set_process(false)
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	check1 = false
-
-func _on_area_2d_2_body_entered(body: Node2D) -> void:
-	check2 = true
-
-func _process(delta):
-	if check1 and check2:
-		area.set_process(false)
-	else:
-		area.set_process(true)
+	area.set_process(true)
