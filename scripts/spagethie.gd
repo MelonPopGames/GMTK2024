e1e55a5d235ae271c51199fe6183f6ccf4d15394
FarhanZaman1895 <117427908+FarhanZaman1895@users.noreply.github.com>
2024-08-20 06:06:23 -0600

    this sure is a game

--- scripts/spagethie.gd
+++ scripts/spagethie.gd
@@ -2,6 +2,7 @@ extends Node
 @onready var area: StaticBody2D = $StaticBody2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("EWH")
 	area.set_process(false)
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
