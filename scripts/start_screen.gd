3e9b78eb92c7d6ac7fd11c88cf6d41f4659dc971
FarhanZaman1895 <117427908+FarhanZaman1895@users.noreply.github.com>
2024-08-20 03:16:58 -0600

    wefhwi

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/game_screens/level_select.tscn")
-
-func _on_button_2_pressed():
-	get_tree().quit()
-
-
 func _on_ready() -> void:
 	MusicPlayer.play_title_music()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://scenes/game_screens/level_select.tscn")
+	elif Input.is_action_just_pressed("LeaveLevel"):
+		get_tree().quit()
