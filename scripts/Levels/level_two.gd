2030c106b0dfcc44c4a349625335f997fa5127f3
Jason Wang <jason.shengxin.wang@gmail.com>
2024-08-20 05:55:37 -0600

    miseyr miserybaidfsdhj

--- scripts/Levels/level_two.gd
+++ scripts/Levels/level_two.gd
@@ -3,5 +3,5 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	MusicPlayer.play_level_music_1()
-	gameManager.gameStart(10)
+	gameManager.gameStart(7)
 	gameManager.currentLevel = 2
