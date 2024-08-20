4de06a5232dbe38ad4e8a4e21c53ccdc97afb422
Jason Wang <jason.shengxin.wang@gmail.com>
2024-08-20 04:33:50 -0600

    level three changes

--- scripts/Levels/level_three.gd
+++ scripts/Levels/level_three.gd
@@ -4,5 +4,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	MusicPlayer.play_level_music_2()
+<<<<<<< Updated upstream
 	gameManager.gameStart(10)
 	gameManager.currentLevel = 3
+=======
+	gameManager.gameStart(5)
+>>>>>>> Stashed changes
